a405f22203c776ab3d66a49f9e280376350b1f1f
Arjun Chelliah <53024410+coolboy56744@users.noreply.github.com>
2023-12-12 19:50:35 -0800

    Add files via upload

--- Game.java
+++ Game.java
@@ -4,30 +4,28 @@ import java.util.Random;
 import java.util.Collections;
 
 public class Game {
-
     private static Scanner Input = new Scanner(System.in);
     private static int bet;
     private static int index;
-
+    private static int sum = 0;
     private static Random random_method = new Random();
-
     private static ArrayList<Integer> cards = new ArrayList<>();
-
-    // private static int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
-
     private static ArrayList<Integer> playerHand = new ArrayList<>();
-
     private static ArrayList<Integer> dealerHand = new ArrayList<>();
 
     Game () {
 
     }
 
+    public static void setIndex(int newIndex) {
+        index = newIndex;
+    }
+
     public static void betting() {
         System.out.println("Welcome to our blackjack game!");
         System.out.println("You currently have " +  FileIO.getScore().get(index) + " points");
 
-        System.out.print("How much would you like ot bet: ");
+        System.out.print("How much would you like to bet: ");
         bet = Input.nextInt();
 
         if (bet > Integer.parseInt(FileIO.getScore().get(index))) {
@@ -38,21 +36,14 @@ public class Game {
         }
     }
 
-
     public static void deck() {
-        // int index = 0;
-        // for (int i = 0; i <= 3; i++) {
-        //     for (int rank = 1; rank <= 13; rank++) {
-        //         cards[index] = values[]);
-        //         index++;
-        //     }
-        // }
-
         int value;
         for(int i = 0; i <= 3; i++) {
             for(int m = 1; m < 14; m++) {
                 if (m > 10) {
                     value = 10;
+                } else if (m == 1) {
+                    value = 11;
                 } else {
                     value = m;
                 }
@@ -63,21 +54,61 @@ public class Game {
         Collections.shuffle(cards);
     }
 
-    public static void setIndex(int newIndex) {
-        index = newIndex;
-    }
-
     public static void startGame() {
-        hit(playerHand);
-        hit(dealerHand);
-        hit(playerHand);
-        hit(dealerHand);
+        betting();
 
-        System.out.println(playerHand);
-        System.out.println(sumOfHand(playerHand));
+        deck();
+
+        hit(playerHand);
+        hit(dealerHand);
+        hit(playerHand);
+        hit(dealerHand);
 
         System.out.println(dealerHand);
-        System.out.println(sumOfHand(dealerHand));
+        System.out.println(playerHand);
+
+        System.out.println("Your cards total a value of " + sumOfHand(playerHand));
+        if (playerHand.contains(11)) {
+            System.out.println("You have an Ace!");
+        }
+        System.out.println("The dealer's first card has a value of " + dealerHand.get(0));
+        if (dealerHand.get(0) == 11) {
+            System.out.println("Its an Ace!");
+        }
+
+        boolean bypassDealer = false;
+
+        while (true) {
+            System.out.println("Would like to hit or stand? (h/s) ");
+            String choice = Input.next();
+
+            while (!choice.equalsIgnoreCase("h") && !choice.equalsIgnoreCase("s")) {
+                System.out.println("Invalid selection");
+                System.out.println("Would like to hit or stand? (h/s) ");
+                choice = Input.next();
+            }
+
+            if (choice.equalsIgnoreCase("h")) {
+                hit(playerHand);
+                System.out.println("Your cards total a value of " + sumOfHand(playerHand));
+                if (sumOfHand(playerHand) > 21) {
+                    bypassDealer = true;
+                    break;
+                }
+            } else if (choice.equalsIgnoreCase("s")) {
+                break;
+            }
+
+        }
+
+        if (!bypassDealer) {
+            System.out.println("The dealer's cards total a value of " + sumOfHand(dealerHand));
+            while (sumOfHand(dealerHand) < 17) {
+                hit(dealerHand);
+                System.out.println("The dealer draws");
+                System.out.println("The dealer's cards now total a value of " + sumOfHand(dealerHand));
+            }
+        }
 
         score(5);
 
@@ -90,13 +121,15 @@ public class Game {
     }
 
     public static void score(int bet) {
-        if (sumOfHand(playerHand) > sumOfHand(dealerHand) || sumOfHand(dealerHand) > 21) {
+        if (sumOfHand(playerHand) > sumOfHand(dealerHand) && sumOfHand(playerHand) <= 21 || sumOfHand(dealerHand) > 21) {
             System.out.println("You win!");
             FileIO.getScore().set(index, String.valueOf(Integer.parseInt(FileIO.getScore().get(index)) + bet * 2));
-        } else if (sumOfHand(playerHand) < sumOfHand(dealerHand) || sumOfHand(playerHand) > 21) {
+        } else if (sumOfHand(playerHand) < sumOfHand(dealerHand) && sumOfHand(dealerHand) <= 21 || sumOfHand(playerHand) > 21) {
             System.out.println("You lose!");
         } else {
             System.out.println("It's a tie!");
+            System.out.println(playerHand);
+            System.out.println(dealerHand);
         }
       
       // playerHand.clear();
@@ -107,10 +140,18 @@ public class Game {
     }
 
     public static int sumOfHand(ArrayList<Integer> hand) {
-        int sum = 0;
-        for (Integer integer : hand) {
+        // ace case in here
+        sum = 0;
+
+        for (int integer : hand) {
             sum += integer;
         }
+
+        if (sum > 21 && hand.contains(11)) {
+            hand.set(hand.indexOf(11), 1);
+            sumOfHand(hand);
+        }
+
         return sum;
     }
 }
